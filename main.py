2502e537afc6028dd1c89fe7658f9c73fd70ca4d
laughing man <komarovandrey396@gmail.com>
2021-10-29 18:36:36 +0300

    training data func added

--- main.py
+++ main.py
@@ -7,4 +7,27 @@ def sigmoid(x):
 def sigmoid_deriv(x):
     return sigmoid(x)*(1-sigmoid(x))
 
+LR = 1   
 
+I_dim = 3
+H_dim = 4
+
+epoch_count = 1
+
+weights_ItoH = np.random.uniform(-1, 1, (I_dim, H_dim))
+weights_HtoO = np.random.uniform(-1, 1, H_dim)
+
+preActivation_H = np.zeroes(H_dim)
+postActivation_H = np.zeroes(H_dim)
+
+training_data = pd.read_excel('MLP_Tdata.xlsx')
+target_output = training_data.output
+training_data = training_data.drop(['output'], axis=1)
+training_data = np.asarray(training_data)
+training_count = len(training_data[:,0])
+
+validation_data = pd.read_excel('MLP_Vdata.xlsx')
+validation_output = validation_data.output
+validation_data = validation_data.drop(['output'], axis=1)
+validation_data = np.asarray(validation_data)
+validation_count = len(validation_data[:,0])
